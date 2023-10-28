6be489fcc770e695f4f1ff08f69713ce579235b2
sirko2a1 <134325906+sirko2a1@users.noreply.github.com>
2023-10-28 14:02:26 +0300

    commit №2

--- currency.py
+++ currency.py
@@ -4,23 +4,26 @@ import argparse
 import datetime
 
 class CurrencyConverter:
-    def __init__(self, api_url, api_key):
+    def __init__(self, api_url):
         self.api_url = api_url
-        self.api_key = api_key
 
     async def fetch_exchange_rates(self, days, currencies):
         async with aiohttp.ClientSession() as session:
             exchange_rates = []
             for day in range(1, min(days, 10) + 1):
                 current_date = (datetime.datetime.now() - datetime.timedelta(days=day)).date()
-                url = f"{self.api_url}/p24api/exchange_rates?json&date={current_date}"
-                async with session.get(url, headers={"Authorization": f"Bearer {self.api_key}"}) as response:
+                current_date_str = current_date.strftime('%d.%m.%Y')
+                url = f"{self.api_url}/p24api/exchange_rates?json&date={current_date_str}"
+                async with session.get(url) as response:
                     if response.status == 200:
                         data = await response.json()
-                        rates = {
-                            current_date.strftime('%d.%m.%Y'): {currency: data[currency] for currency in currencies}
-                        }
-                        exchange_rates.append(rates)
+                        exchangeRate = data.get('exchangeRate')
+                        rates = {}
+                        for rate in exchangeRate:
+                            currency = rate.get("currency")
+                            if currency in currencies:
+                                rates[currency] = f"SALE {rate.get('saleRateNB')} BUY {rate.get('purchaseRateNB')}"
+                        exchange_rates.append({current_date_str: rates})
             return exchange_rates
 
 def main():
@@ -32,10 +35,9 @@ def main():
     days = args.days
     currencies = args.currencies
 
-    api_url = "https://api.privatbank.ua/#p24/exchangeArchive"
-    api_key = "YOUR_API_KEY"
+    api_url = "https://api.privatbank.ua"
 
-    converter = CurrencyConverter(api_url, api_key)
+    converter = CurrencyConverter(api_url)
 
     loop = asyncio.get_event_loop()
     exchange_rates = loop.run_until_complete(converter.fetch_exchange_rates(days, currencies))
@@ -45,4 +47,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
